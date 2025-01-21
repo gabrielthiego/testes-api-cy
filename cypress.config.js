cd53f6a0c23ce463d25b3862ce56ba3f2c59ab5e
Gabriel Thiego <gabrielthiego@gmail.com>
2025-01-21 20:07:11 -0300

    desfazendo conflitos

--- cypress.config.js
+++ cypress.config.js
@@ -5,16 +5,16 @@ module.exports = defineConfig({
   reporterOptions: {
     reportDir: 'mochawesome-report',
     overwrite: false,
-    reportFilename: "index.html", // Nome do arquivo de saída
-    html: true, // Gera o relatório em HTML
-    json: false, // Não gera o arquivo JSON
+    reportFilename: "index.html", 
+    html: true, 
+    json: false, 
   },
   e2e: {
-    // Configurações de plugins (se necessário)
+    
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: 'http://localhost:3000/', // URL base para os testes
+    baseUrl: 'http://localhost:3000/',
   },
 })
 
