3bec36b360ea05e1dcf8e0484c40ba314c8e3aaf
Gabriel Thiego <gabrielthiego@gmail.com>
2025-01-21 20:11:36 -0300

    ci config

--- cypress.config.js
+++ cypress.config.js
@@ -10,11 +10,9 @@ module.exports = defineConfig({
     json: false, 
   },
   e2e: {
-    
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'http://localhost:3000/',
   },
 })
-
