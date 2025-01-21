1b5089d4f655539a63e8a74fd7db4d18376a2836
Gabriel Thiego <gabrielthiego@gmail.com>
2025-01-21 20:48:03 -0300

    actions config 26

--- cypress.config.js
+++ cypress.config.js
@@ -16,3 +16,20 @@ module.exports = defineConfig({
     baseUrl: 'http://localhost:3000/',
   },
 })
+// cypress.config.js
+module.exports = {
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'mochawesome-report',
+    overwrite: false,
+    reportFilename: "index.html",
+    html: true,
+    json: false,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    },
+    baseUrl: 'http://localhost:3000/',
+  },
+}
